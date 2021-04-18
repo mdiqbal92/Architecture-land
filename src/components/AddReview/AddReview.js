f813a53cd0ef396c044668377fbc85d012a55255
Md Ibal Hossain <iqbal.h1992@gmail.com>
2021-04-18 23:32:36 +0600

    review

--- src/components/AddReview/AddReview.js
+++ src/components/AddReview/AddReview.js
@@ -9,8 +9,9 @@ const AddReview = () => {
 
     const onSubmit = data => {
         const ServiceData = {
+            quote: data.quote,
             name: data.name,
-            price: data.price,
+            from: data.from,
             imageURL: imageURL
         }
         const url = `https://fast-lake-38919.herokuapp.com/addReview`;
@@ -43,7 +44,7 @@ const AddReview = () => {
         <div className ="container-fluid row">
             <Sidebar></Sidebar>
             <div className="col-md-10 p-4 pr-5" style={{ position: "absolute", right: 0, backgroundColor: "#F4FDFB" }}>
-                <h3>Add a New Service here</h3>
+                <h3>Want to review us? </h3>
                 <form style={{ margin: '40px' }} className="container" onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="name">Quote</label>
                     <input className="form-control" name="quote" ref={register} /><br />
